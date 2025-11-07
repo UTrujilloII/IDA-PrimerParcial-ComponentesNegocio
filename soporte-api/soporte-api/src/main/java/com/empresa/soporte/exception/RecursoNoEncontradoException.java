44de26a4346183245b679f01c7c2fb0e05114d64
LuisRodriguez2812 <a19104795@idat.pe>
2025-11-07 01:00:25 -0500

    Delete soporte-api/soporte-api directory

--- soporte-api/soporte-api/src/main/java/com/empresa/soporte/exception/RecursoNoEncontradoException.java
+++
@@ -1,5 +0,0 @@
-package com.empresa.soporte.exception;
-
-public class RecursoNoEncontradoException extends RuntimeException {
-    public RecursoNoEncontradoException(String mensaje) { super(mensaje); }
-}
